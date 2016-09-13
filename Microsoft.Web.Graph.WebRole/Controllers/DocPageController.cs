729382f0329b6edf44a06c6fa3403d152cfb0e4e
jinlinxu <jnlxu@microsoft.com>
2016-09-12 23:32:57 -0700

    Add the code to serve one doc page through controller

---
+++ Microsoft.Web.Graph.WebRole/Controllers/DocPageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DynamicDocs.Util;
+using DynamicDocs.Models;
+
+namespace DynamicDocs.Controllers
+{
+    public class DocPageController: Controller
+    {
+        public ActionResult GetDocPage(string docPath)
+        {
+            DocMeta model = new DocMeta();
+                            model.DocToc = TocManager.GetToc();
+                model.CurrentDocSets = TocManager.GetDocSets();
+            if (docPath.ToLower() == "/docs/overview")
+            {
+
+                model.InnerContent = TocManager.ReadOnlineFile("https://ashirstest.blob.core.windows.net/dynamicdoc/office-add-ins.htm");
+            }
+            else
+            {
+                model.InnerContent = "404 not found.";
+            }
+            HttpContext.Response.ContentType = "text/fdxml";
+            return View(model);
+        }
+    }
+}
---
+++ Microsoft.Web.Graph.WebRole/Models/DocMeta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicDocs.Models
+{
+    public class DocMeta
+    {
+        public Toc DocToc { get; set; }
+
+        public DocSets CurrentDocSets { get; set; }
+
+        public string InnerContent { get; set; }
+    }
+}
