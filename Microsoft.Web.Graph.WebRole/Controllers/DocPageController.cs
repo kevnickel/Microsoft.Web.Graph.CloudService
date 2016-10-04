f5283a8c9fa5dd0e69832ce472f9d2518a3eea70
Ashirvad Sahu <ashirs@microsoft.com>
2016-10-04 14:16:11 -0700

    Use the culture service in controller isntead of directly dealing with culture

--- Microsoft.Web.Graph.WebRole/Controllers/DocPageController.cs
+++ Microsoft.Web.Graph.WebRole/Controllers/DocPageController.cs
@@ -5,17 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.Web.Graph.WebRole.Util;
 using DynamicDocs.Models;
+using Microsoft.Web.Portal.Common.Services;
 
 namespace Microsoft.Web.Graph.WebRole.Controllers
 {
     public class DocPageController : BaseController
     {
+        private ICultureService _cultureService = null;
+
+        public DocPageController(ICultureService cultureService)
+        {
+            _cultureService = cultureService;
+        }
         public ActionResult GetDocPage(string culture, string docPath)
         {
             DocMeta model = new DocMeta();
-            model.DocToc = DocContentManager.GetToc(culture, docPath);
-            model.CurrentDocSets = DocContentManager.GetDocSets(culture, docPath);
-            model.InnerContent = DocContentManager.GetDocContent(culture, docPath);
+            model.DocToc = DocContentManager.GetToc(_cultureService.CurrentCulture, docPath);
+            model.CurrentDocSets = DocContentManager.GetDocSets(_cultureService.CurrentCulture, docPath);
+            model.InnerContent = DocContentManager.GetDocContent(_cultureService.CurrentCulture, docPath);
             model.DocPath = DocContentManager.GetDocShortPath(docPath);
             PreProcessDocModel(model);
             return View(model);
@@ -29,13 +36,13 @@ namespace Microsoft.Web.Graph.WebRole.Controllers
 
         public ContentResult ClearTocCache(string culture, string productCategory, string docSetCategory)
         {
-            if (string.IsNullOrEmpty(culture) || string.IsNullOrEmpty(productCategory) || string.IsNullOrEmpty(docSetCategory))
+            if (string.IsNullOrEmpty(_cultureService.CurrentCulture) || string.IsNullOrEmpty(productCategory) || string.IsNullOrEmpty(docSetCategory))
             {
                 return Content("Input paramters are incorrect, please follow below example:<br/>docpage/clearTocCache?cluture=en-us&productCategory=add-ins&docSetCategory=reference");
             }
             else
             {
-                DocContentManager.ClearTocCache(culture, productCategory, docSetCategory);
+                DocContentManager.ClearTocCache(_cultureService.CurrentCulture, productCategory, docSetCategory);
                 return Content("Specifed Toc Cache cleared");
             }
         }
@@ -48,7 +55,7 @@ namespace Microsoft.Web.Graph.WebRole.Controllers
 
         public ContentResult ClearDocSetsCache(string culture, string productCategory)
         {
-            DocContentManager.ClearDocSetsCache(culture, productCategory);
+            DocContentManager.ClearDocSetsCache(_cultureService.CurrentCulture, productCategory);
             return Content("Sepcified docsets Cache cleared");
         }
 
