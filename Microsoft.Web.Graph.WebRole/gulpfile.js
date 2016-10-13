f78bbcc7cbfa6f91d556bbb90397459df44eeee0
Kevin Nickel <kevnickel@hotmail.com>
2016-10-13 15:48:30 -0700

    Add sass task to the deploy task

--- Microsoft.Web.Graph.WebRole/gulpfile.js
+++ Microsoft.Web.Graph.WebRole/gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('default', function () {
     // place code for your default task here
 });
 
-gulp.task('deploy', ['css']);
+gulp.task('deploy', ['sass','css']);
 
 gulp.task('sass', function () {
     gulp.src(sassSourceFiles)
