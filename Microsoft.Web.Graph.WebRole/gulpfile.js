c38aa2ddda0aba026138cd12cefc2f51453af4d8
Kevin Nickel <kevnickel@hotmail.com>
2016-10-14 16:24:51 -0700

    Clean up gulpfile

--- Microsoft.Web.Graph.WebRole/gulpfile.js
+++ Microsoft.Web.Graph.WebRole/gulpfile.js
@@ -4,8 +4,20 @@ This file in the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var sassSourceFiles = ['Content/src/*.scss'];
-var cssDestFolder = 'Content/build';
+// Location of files
+var paths = {
+    css: 'Content/build/css',
+    scripts: 'Scripts/build/js'
+};
+
+// Lists of files to work with
+var sources = {
+    sass: 'Content/src/**/*.scss',
+    css: [
+        paths.css + '/**/*.css',
+        '!' + paths.css + 'main.css'
+    ]
+};
 
 var gulp = require('gulp');
 
@@ -18,28 +30,31 @@ gulp.task('default', function () {
     // place code for your default task here
 });
 
+// Task to run during site deployment
 gulp.task('deploy', function(callback) {
     plugins.runSequence('sass',
         ['css'],
         callback);
 });
 
+// Create css from sass files
 gulp.task('sass', function () {
-    gulp.src(sassSourceFiles)
+    gulp.src(sources.sass)
         .pipe(plugins.sass())
-        .pipe(gulp.dest(cssDestFolder));
+        .pipe(gulp.dest(paths.css));
 });
 
+// Bundle and minify the css
 gulp.task('css', function () {
     var cssOutputFile = 'main.css';
-    var cssFiles = [cssDestFolder + '/*.css', '!' + cssDestFolder + '/' + cssOutputFile];
 
-    gulp.src(cssFiles)
+    gulp.src(sources.css)
 		.pipe(plugins.concat(cssOutputFile))
         .pipe(plugins.cleanCss({ compatibility: 'ie8' }))
-		.pipe(gulp.dest(cssDestFolder));
+		.pipe(gulp.dest(paths.css));
 });
 
+// Build sass on save
 gulp.task('watch', function() {
-    gulp.watch(sassSourceFiles, ['sass']);
+    gulp.watch(sources.sass, ['sass']);
 });
--- deploy.cmd
+++ deploy.cmd
@@ -129,7 +129,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-echo 5. Build to the temporary path
+echo 4. Build to the temporary path
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\\Microsoft.Web.Graph.WebRole\\Microsoft.Web.Graph.WebRole.csproj" /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 ) ELSE (
@@ -138,13 +138,13 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 
 IF !ERRORLEVEL! NEQ 0 goto error
 
-echo 6. KuduSync
+echo 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-echo 4. Run gulp tasks
+echo 6. Run gulp tasks
 echo "%DEPLOYMENT_TARGET%"
 IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
